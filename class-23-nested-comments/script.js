0b75ece1e9ed39480372924cfe39ce765ee44c53
SBagaria2710 <shashwawtforwork@gmail.com>
2026-01-16 22:02:08 +0530

    class-23 nested comments

---
+++ class-23-nested-comments/script.js
@@ -0,0 +1,77 @@
+const commentContainer = document.getElementById("commentContainer");
+
+const createElement = (elementName = "div", attributes = {}, ...children) => {
+    // Create Element
+    const element = document.createElement(elementName);
+
+    // Add Attributes
+    for (const key in attributes) {
+        element[key] = attributes[key];
+    }
+
+    // Append Children
+    children.forEach((child) => element.appendChild(child));
+
+    return element;
+};
+
+const createComment = (name, text) => {
+    const p1 = createElement("p", { innerText: name });
+    const p2 = createElement("p", { innerText: text });
+    const replyBtn = createElement("button", {
+        innerText: "Reply",
+        className: "reply"
+    });
+
+    const mainComment = createElement("div", { className: "main-comment" }, p1, p2, replyBtn);
+    const subCommentContainer = createElement("div", {
+        className: "sub-comment-container"
+    });
+
+    const comment = createElement("div", { className: "comment" }, mainComment, subCommentContainer);
+
+    return comment;
+}
+
+const createCommentInput = () => {
+    const nameInput = createElement("input", {
+        type: "text",
+        placeholder: "Name",
+        className: "text-name name"
+    });
+    const commentInput = createElement("textarea", {
+        rows: 2,
+        cols: 40,
+        placeholder: "Comment",
+        className: "comment-text"
+    })
+
+    const postBtn = createElement("button", {
+        innerText: "Post",
+        className: "post"
+    });
+
+    const cancelBtn = createElement("button", {
+        innerText: "Cancel",
+        className: "cancel"
+    });
+
+    const btnHolder = createElement("div", {
+        className: "btn-holder"
+    }, postBtn, cancelBtn);
+
+    const commentInputContainer = createElement("div", { className: "comment-input-container" }, nameInput, commentInput, btnHolder);
+
+    return commentInputContainer;
+};
+
+const firstComment = createComment("Shashwat", "Let's Start!");
+commentContainer.append(firstComment);
+
+commentContainer.addEventListener("click", function (event) {
+    const clickedElement = event.target;
+
+    if (clickedElement.tagName === "BUTTON") {
+        console.log("A button was clicked!");
+    }
+});
