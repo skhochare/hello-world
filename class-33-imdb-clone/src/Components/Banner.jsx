0f0388a3c82efc07ad78449a6c26f4cb53fbee28
Abhishek <abhishekgoel@gofynd.com>
2026-02-20 21:56:25 +0530

    apinfetch

--- class-33-imdb-clone/src/Components/Banner.jsx
+++ class-33-imdb-clone/src/Components/Banner.jsx
@@ -6,9 +6,10 @@ function Banner() {
     "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
   );
   const [title, setTitle] = useState("Placeholder Title");
-  console.log("banner");
+
   useEffect(() => {
-    const options = {
+    function fetchData(){
+      const options = {
       method: "GET",
       url: "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
       headers: {
@@ -17,22 +18,24 @@ function Banner() {
           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzQ5ZWU4NjkyN2M4NjJlNmFjNDAzNjBlM2ViOGMwZCIsIm5iZiI6MTY1NzgxODcwMy4yMDIsInN1YiI6IjYyZDA0ZTRmMzk0YTg3MDRhZTVjNWEzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._LNDpBJ--YTga2vupX46hCWhBnsgEW43JjSJ2hyTA6k",
       },
     };
-    console.log("useEffect banner");
-    
     axios
       .request(options)
       .then((res) => {
-        console.log("hello");
-        
-        console.log(res.data)})
+        console.log(res.data);
+        const movie = res.data?.results[0];
+        setBannerImage(movie["backdrop_path"]);
+        setTitle(movie["title"]);
+      })
       .catch((err) => console.error(err));
+    }
+    fetchData();
   }, []);
 
   return (
     <>
       <div
         className="h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end"
-        style={{ backgroundImage: `url(${bannerImage})` }}
+        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${bannerImage})` }}
       >
         <div className="text-white w-full text-center text-2xl">{title}</div>
       </div>
---
+++ class-33-imdb-clone/src/Components/Movies.jsx
@@ -0,0 +1,69 @@
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+function Movies() {
+    const [movies, setMovies] = useState([
+        {url:"https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68", title:"Movie 1"},
+         {url:"https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68", title:"Movie 2"},
+         {url:"https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68", title:"Movie 3"},
+         {url:"https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68", title:"Movie 4"},
+         {url:"https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68", title:"Movie 5"},
+     ]);
+     const [pageNo, setPageNo] = useState(1);
+     const handlePrev = () => {
+        if(pageNo!=1) setPageNo(pageNo-1);
+     }
+
+     const handleNext = () => {
+        setPageNo(pageNo+1);
+     }
+
+       useEffect(() => {
+    function fetchData(){
+      const options = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzQ5ZWU4NjkyN2M4NjJlNmFjNDAzNjBlM2ViOGMwZCIsIm5iZiI6MTY1NzgxODcwMy4yMDIsInN1YiI6IjYyZDA0ZTRmMzk0YTg3MDRhZTVjNWEzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._LNDpBJ--YTga2vupX46hCWhBnsgEW43JjSJ2hyTA6k",
+      },
+    };
+    axios
+      .request(options)
+      .then((res) => {
+        const movie = res.data?.results;
+        setMovies(movie);
+      })
+      .catch((err) => console.error(err));
+    }
+    fetchData();
+  }, []);
+  return (
+    <div>
+        {/* title */}
+        <div className='text-2xl font-bold text-center m-5'>
+            <h1>Trending Movies</h1>
+        </div>
+
+        {/* all movies */}
+        <div className='flex justify-evenly flex-wrap gap-8'>
+            {movies.map((movieObj) =>{
+                return (
+                <div className="h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer" style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movieObj["poster_path"]})`}}>
+                    <div className='text-white w-full text-center text-xl bg-gray-900/70 rounded-xl p-2'>{movieObj.title}</div>
+                </div>)
+            })}
+        </div>
+
+        {/* pagination */}
+        <div className='bg-gray-400 h-[50px] w-full mt-8 flex justify-center gap-3 p-4'>
+            {/* dont show if pageNo is 0  */}
+            <div onClick={handlePrev}><i class="fa-solid fa-left-long"></i></div>
+            <div>{pageNo}</div>
+            <div onClick={handleNext}><i class="fa-solid fa-right-long"></i></div>
+        </div>
+    </div>
+  )
+}
+
+export default Movies
